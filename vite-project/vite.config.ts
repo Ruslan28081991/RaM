a8bf88d04c49bb93c547c1b91f95f50256eb777c
Ruslan <vich_91@mail.ru>
2026-03-18 18:03:37 +0300

    Проверка

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/RaM/',
+  base: '/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
